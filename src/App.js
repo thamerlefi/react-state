abaead0aa4ac614164dcf43d1e94ce061bc9b2ca
thamer <themerlefi@gmail.com>
2023-04-17 01:21:41 +0100

    end

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 
 class App extends Component {
+  // state
   state = {
     person: {
       fullName: "Lefi Thamer",
@@ -13,40 +14,42 @@ class App extends Component {
     mountTime: new Date(),
     intervalId: null,
   };
-
+  // life cycle
   componentDidMount() {
     const intervalId = setInterval(() => {
       this.setState({ mountTime: new Date() });
     }, 1000);
     this.setState({ intervalId, mountTime: new Date() });
+    console.log('mounted')
   }
-
 
   componentWillUnmount() {
     clearInterval(this.state.intervalId);
+    console.log('unmounted')
   }
-
+  // toggle show profile function
   toggleShow = () => {
     this.setState({ shows: !this.state.shows });
   };
 
   render() {
+    // state destructuring
     const { fullName, bio, imgSrc, profession } = this.state.person;
     const timeSinceMount = Math.floor(
       ( this.state.mountTime - this.state.fixTime) / 1000
     );
     return (
       <div className="container">
-        <button onClick={this.toggleShow}>Toggle Profile</button>
+        <button onClick={this.toggleShow}>{`${this.state.shows ? 'hide':'show'} Profile`}</button>
         {this.state.shows && (
           <div className="profile">
-            <img src={imgSrc} alt={fullName} style={{width:150, height:150}}/>
             <h3>{fullName}</h3>
+            <img src={imgSrc} alt={fullName} style={{width:150, height:150}}/>
             <p>{bio}</p>
             <h3>{profession}</h3>
           </div>
         )}
-        <p>Mounted {timeSinceMount} seconds ago</p>
+        <p className="time">Mounted {timeSinceMount} seconds ago</p>
       </div>
     );
   }
